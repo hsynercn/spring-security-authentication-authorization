918a63a437483363fc2cf06b18fa01a32082b508
hsynercn <huscanercan@gmail.com>
2020-09-24 22:05:48 +0300

    Simplify controller

--- My Login Sandbox/src/main/java/com/mylogin/login/controller/ResourceController.java
+++ My Login Sandbox/src/main/java/com/mylogin/login/controller/ResourceController.java
@@ -1,36 +1,13 @@
 package com.mylogin.login.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-
-@Controller
+@RestController
 public class ResourceController {
-
-    private List<String> tasks = Arrays.asList("This", "is", "not", "a", "banana");
-
     @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("message", LocalDateTime.now().toString());
-        model.addAttribute("tasks", tasks);
-
-        return "welcome"; //view
-    }
-
-    // /hello?name=kotlin
-    @GetMapping("/hello")
-    public String mainWithParam(
-            @RequestParam(name = "name", required = false, defaultValue = "")
-                    String name, Model model) {
-
-        model.addAttribute("message", LocalDateTime.now().toString());
-
-        return "welcome"; //view
+    public String main() {
+        return "HELLO WORLD";
     }
 
 }
